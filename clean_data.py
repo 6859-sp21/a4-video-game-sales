cde03f8ad54c308adb8bf43b0e822039c0790314
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2021-03-30 12:44:32 -0400

    Fix cleaning script to not write index

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,6 @@ data_filt = data \
     .dropna(subset=['Year', 'Publisher', 'Global_Sales']) \
     .reset_index(drop=True)
 
-data_filt.to_csv('vgsales_clean.csv')
+data_filt.to_csv('vgsales_clean.csv', index=False)
 
 print('cleaning complete. see vgsales_clean.csv')
