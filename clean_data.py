458c5e8dfe7b2c781cfaaedfb6db79d7e9d941dd
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2021-04-05 21:41:21 -0400

    Add latest data cleaning code and files

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,41 @@ data_filt = data \
     .dropna(subset=['Year', 'Publisher', 'Global_Sales']) \
     .reset_index(drop=True)
 
-data_filt.to_csv('vgsales_clean.csv', index=False)
+# map the Platform field
+platform_map = {
+    'NES': 'Nintendo Entertainment System',
+    'GB': 'Game Boy',
+    'DS': 'Nintendo DS',
+    'X360': 'Xbox 360',
+    'PS3': 'PlayStation 3',
+    'PS2': 'PlayStation 2',
+    'SNES': 'Super Nintendo Entertainment System',
+    'GBA': 'Game Boy Advance',
+    '3DS': 'Nintendo 3DS',
+    'PS4': 'PlayStation 4',
+    'N64': 'Nintendo 64',
+    'PS': 'PlayStation',
+    'XB': 'Xbox',
+    '2600': 'Atari 2600',
+    'PSP': 'PlayStation Portable',
+    'XOne': 'Xbox One',
+    'GC': 'GameCube',
+    'WiiU': 'Wii U',
+    'GEN': 'Sega Genesis',
+    'DC': 'Dreamcast',
+    'PSV': 'PlayStation Vita',
+    'SAT': 'Sega Saturn',
+    'SCD': 'Sega CD',
+    'WS': 'WonderSwan',
+    'NG': 'Neo Geo',
+    'TG16': 'TurboGrafx-16',
+    '3DO': '3DO Interactive Multiplayer',
+    'GG': 'Game Gear',
+    'PCFX': 'PC-FX'
+}
+data_filt.Platform = data_filt.Platform.map(platform_map)
 
-print('cleaning complete. see vgsales_clean.csv')
+data_filt.to_csv('vgsales_clean_2.csv', index=False)
+data_filt.to_json('vgsales_clean_2.json', orient='records')
+
+print('cleaning complete. see csv and json files.')
