c92c7f977494656c9f5a013c908cc47b2da0f128
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2021-04-14 01:48:08 -0400

    Add latest bar chart work

--- d3/bar.js
+++ d3/bar.js
@@ -1,90 +1,60 @@
 const dataUrl = 'https://raw.githubusercontent.com/6859-sp21/a4-video-game-sales/main/vgsales_clean_2.csv'
 
-d3.csv(dataUrl, d3.autoType).then(data => {
-  // convert data to key and value format
-  // let's sum sales by platform
-  const platformSales = {}
-  data.forEach(d => {
-    const platform = d.Platform
-    const sales = d.Global_Sales
-    if (platform in platformSales) {
-      platformSales[platform] += sales
-    } else {
-      platformSales[platform] = sales
-    }
-  })
-
-  // convert to list of name, value objects
-  const platformSalesData = Object.keys(platformSales)
-    .map(platform => ({
-      name: platform,
-      value: platformSales[platform],
-    }))
-    .sort((a, b) => b.value - a.value)
-    .slice(0, 10)
-    .map((vals, i) => ({
-      ...vals,
-      index: i,
-    }))
-
+const barChart = ({data, chartKey, x, y, isClickable, onMouseOver, onMouseLeave, onClick}) => {
   // bar chart time!
   // first, some housekeeping
-  const margin = ({top: 30, right: 30, bottom: 10, left: 150})
-  const width = 700
-  const height = 400
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(platformSalesData, d => d.value)])
+  const margin = ({top: 30, right: 30, bottom: 10, left: 500})
+  const width = 1000
+  const height = 300
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d[x])])
     .range([margin.left, width - margin.right])
-  const y = d3.scaleBand()
-    .domain(d3.range(platformSalesData.length))
+  const yScale = d3.scaleBand()
+    .domain(d3.range(data.length))
     .rangeRound([margin.top, height - margin.bottom])
     .padding(0.1)
   const colorScale = d3.scaleOrdinal()
-    .domain(platformSalesData.map(d => d.name))
+    .domain(data.map(d => d[y]))
     .range(d3.schemeTableau10)
   
-    const format = x.tickFormat(20, platformSalesData.format)
+  const format = xScale.tickFormat(20, data.format)
   const xAxis = g => g
     .attr('transform', `translate(0, ${margin.top})`)
-    .call(d3.axisTop(x).ticks(width / 80, platformSalesData.format))
+    .call(d3.axisTop(xScale).ticks(width / 80, data.format))
     .call(g => g.select('.domain').remove())
   const yAxis = g => g
     .attr('transform', `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y).tickFormat(i => platformSalesData[i].name).tickSizeOuter(0))
+    .call(d3.axisLeft(yScale).tickFormat(i => data[i].name).tickSizeOuter(0))
 
   const svg = d3.create('svg')
     .attr('viewBox', [0, 0, width, height])
   
-  svg.append('g')
+  const sel = svg.append('g')
     .selectAll('rect')
-    .data(platformSalesData)
-    .join('rect')
-    .attr('x', x(0))
-    .attr('y', (d, i) => y(i))
-    .attr('width', d => x(d.value) - x(0))
-    .attr('height', y.bandwidth())
-    .attr('fill', d => colorScale(d.name))
-    .attr('class', 'bar')
-    .attr('id', d => `bar-${d.index}`)
-    .on('mouseover', (e, d) => {
-      d3.selectAll('.bar')
-        .transition()
-      d3.selectAll('.bar')
-        .style('opacity', 0.5)
-      d3.selectAll(`#bar-${d.index}`)
-        // .transition()
-        // .delay(100)
-        .style('opacity', 1)
-    })
-    .on('mouseleave', () => {
-      d3.selectAll('.bar')
-        .transition()
-        .delay(100)
-        .style('opacity', 1)
-    })
+    .data(data)
+  
+  sel.exit().remove()
+    
+    
+  sel.enter().append("rect")
 
-  // handle dynamic opacity
-  d3.select(`#bar`)
+  sel.join('rect')
+    .attr('x', xScale(0))
+    .attr('y', (d, i) => yScale(i))
+    .attr('width', d => xScale(d.value) - xScale(0))
+    .attr('height', yScale.bandwidth())
+    .attr('fill', d => colorScale(d.name))
+    .attr('class', `bar-${chartKey}`)
+    .attr('id', d => `bar-${chartKey}-${d.index}`)
+    .on('mouseover', (e, d) => {
+      onMouseOver(d)
+    })
+    .on('mouseleave', (e, d) => {
+      onMouseLeave(d)
+    })
+    .on('click', (event, d) => {
+      onClick(d)
+    })
 
   // add text labels to bar chart
   svg.append("g")
@@ -93,15 +63,15 @@ d3.csv(dataUrl, d3.autoType).then(data => {
       .attr("font-family", "sans-serif")
       .attr("font-size", 12)
     .selectAll("text")
-    .data(platformSalesData)
+    .data(data)
     .join("text")
-      .attr("x", d => x(d.value))
-      .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+      .attr("x", d => xScale(d.value))
+      .attr("y", (d, i) => yScale(i) + yScale.bandwidth() / 2)
       .attr("dy", "0.35em")
       .attr("dx", -4)
       .attr("font-weight", "bold")
       .text(d => format(d.value))
-    .call(text => text.filter(d => x(d.value) - x(0) < 20) // short bars
+    .call(text => text.filter(d => xScale(d.value) - xScale(0) < 20) // short bars
       .attr("dx", +4)
       .attr("fill", "black")
       .attr("text-anchor", "start"));
@@ -111,12 +81,210 @@ d3.csv(dataUrl, d3.autoType).then(data => {
   
   svg.append('g')
     .call(yAxis)
+    .style('font-size', '16px')
 
-  // handle clicking
-  svg.selectAll('.bar')
-    .on('click', (event, d) => {
-      console.log(d)
+  return { svg, colorScale }
+}
+
+const sumByCol = (data, groupCol, dataCol) => {
+  const result = {}
+  data.forEach(d => {
+    const group = d[groupCol]
+    const value = d[dataCol]
+    if (group in result) {
+      result[group] += value
+    } else {
+      result[group] = value
+    }
+  })
+
+  // convert to list of name, value, index objects
+  const resultArray = Object.keys(result)
+    .map(group => ({
+      name: group,
+      value: result[group],
+    }))
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 10)
+    .map((vals, i) => ({
+      ...vals,
+      index: i,
+    }))
+  
+  return resultArray
+}
+
+d3.csv(dataUrl, d3.autoType).then(data => {
+  // TODO these will be passed to me from others
+  const startYear = 2002
+  const endYear = 2012
+  const genres = ['Action', 'Adventure', 'Puzzle']
+
+  // filter the data
+  const dataFilt = data.filter(d => {
+    return startYear <= d.Year && d.Year <= endYear && genres.indexOf(d.Genre) > -1
+  })
+
+  // aggregate
+  let publisherSalesData = sumByCol(dataFilt, 'Publisher', 'Global_Sales')
+  let gameSalesData = sumByCol(dataFilt, 'Name', 'Global_Sales')
+  
+  // games chart
+  let selectedGame = null
+  const gameOnMouseOver = d => {
+    d3.selectAll('.bar-game')
+      .transition()
+    d3.selectAll('.bar-game')
+      .filter(d => ! selectedGame || (selectedGame && selectedGame.index !== d.index))
+      .style('opacity', 0.3)
+    d3.selectAll(`#bar-game-${d.index}`)
+      .style('opacity', 1)
+      .style('cursor', 'pointer')
+  }
+  const gameOnMouseLeave = d => {
+    d3.selectAll('.bar-game')
+      .filter(d => selectedGame && selectedGame.index === d.index)
+      .transition()
+      .delay(100)
+      .style('opacity', 1)
+    if (selectedGame) {
+      d3.selectAll('.bar-game')
+        .filter(d => selectedGame && selectedGame.index !== d.index)
+        .transition()
+        .delay(100)
+        .style('opacity', 0.3)
+    }
+  }
+  const gameOnClick = d => {
+    selectedGame = d
+    d3.selectAll('#selected-game')
+      .text(d.name)
+      .style('color', colorScaleGames(selectedGame.name))
+      .text(selectedGame.name)
+    d3.selectAll('.bar-game')
+      .filter(d => selectedGame && selectedGame.index !== d.index)
+      .style('opacity', 0.3)
+  }
+  let { svg: gameChart, colorScale: colorScaleGames } = barChart({
+    data: gameSalesData,
+    chartKey: 'game',
+    x: 'value',
+    y: 'name',
+    isClickable: true,
+    onMouseOver: gameOnMouseOver,
+    onMouseLeave: gameOnMouseLeave,
+    onClick: gameOnClick,
+  })
+
+  // publisher chart
+  let selectedPublisher = null
+  const publisherOnMouseOver = d => {
+    d3.selectAll('.bar-publisher')
+      .transition()
+    d3.selectAll('.bar-publisher')
+      .filter(d => ! selectedPublisher || (selectedPublisher && selectedPublisher.index !== d.index))
+      .style('opacity', 0.3)
+    d3.selectAll(`#bar-publisher-${d.index}`)
+      .style('opacity', 1)
+      .style('cursor', 'pointer')
+  }
+  const publisherOnMouseLeave = d => {
+    d3.selectAll('.bar-publisher')
+      .filter(d => selectedPublisher && selectedPublisher.index === d.index)
+      .transition()
+      .delay(100)
+      .style('opacity', 1)
+    if (selectedPublisher) {
+      d3.selectAll('.bar-publisher')
+        .filter(d => selectedPublisher && selectedPublisher.index !== d.index)
+        .transition()
+        .delay(100)
+        .style('opacity', 0.3)
+    }
+  }
+  const publisherOnClick = d => {
+    selectedPublisher = d
+    d3.selectAll('#selected')
+      .text(d.name)
+      .style('color', colorScale(selectedPublisher.name))
+      .text(selectedPublisher.name)
+    d3.selectAll('.bar')
+      .filter(d => selectedPublisher && selectedPublisher.index !== d.index)
+      .style('opacity', 0.3)
+    
+    // re-filter
+    const gamesFilt = dataFilt.filter(d => d.Publisher === selectedPublisher.name)
+    gameSalesData = sumByCol(gamesFilt, 'Name', 'Global_Sales')
+
+    // clear selectedGame
+    selectedGame = null
+
+    // update games chart
+    // gameChart.selectAll('rect').data(gamesFilt).join()
+
+    document.getElementById('one').removeChild(gameChart.node())
+    const newChart = barChart({
+      data: gameSalesData,
+      chartKey: 'game',
+      x: 'value',
+      y: 'name',
+      isClickable: true,
+      onMouseOver: gameOnMouseOver,
+      onMouseLeave: gameOnMouseLeave,
+      onClick: gameOnClick,
+    })
+    gameChart = newChart.svg
+    document.getElementById('one').appendChild(gameChart.node())
+  }
+  const { svg: publisherChart, colorScale } = barChart({
+    data: publisherSalesData,
+    chartKey: 'publisher',
+    x: 'value',
+    y: 'name',
+    isClickable: true,
+    onMouseOver: publisherOnMouseOver,
+    onMouseLeave: publisherOnMouseLeave,
+    onClick: publisherOnClick,
+  })
+
+  // add button
+  d3.select('#one')
+    .append("input")
+    .attr("type", "button")
+    .attr("class", "button")
+    .attr("value", "Reset")
+    .on('click', () => {
+      selectedPublisher = null
+      selectedGame = null
+      // reset all opacities
+      d3.selectAll('.bar-publisher,.bar-game')
+        .transition()
+        .delay(100)
+        .style('opacity', 1)
+      
+      // delete text
+      d3.selectAll('.selected')
+        .text('None')
+        .style('color', 'black')
     })
   
-  document.getElementById('one').appendChild(svg.node())
+  document.getElementById('one').appendChild(publisherChart.node())
+
+  d3.select('#one')
+    .append('h1')
+    .attr('id', 'selected')
+    .attr('class', 'selected')
+    .style('font-family', 'sans-serif')
+    .text('None')
+    .style('color', 'black')
+  
+  document.getElementById('one').appendChild(gameChart.node())
+
+  d3.select('#one')
+    .append('h1')
+    .attr('id', 'selected-game')
+    .attr('class', 'selected')
+    .style('font-family', 'sans-serif')
+    .text('None')
+    .style('color', 'black')
 })
