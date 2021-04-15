49f1800aed78716bcfb18bfa076c1326e3cacdaa
christian moroney <cmoroney@mit.edu>
2021-04-15 14:07:39 -0400

    Jack Christian and Kevin got bubble chart tooltip in better spot

--- d3/charts/stacked_chart.js
+++ d3/charts/stacked_chart.js
@@ -71,6 +71,14 @@ function genreArrayRemove(arr, value) {
     });
 }
 
+function formTooltipString(value) {
+    if (value < 0.1)
+    {
+        return "Global Sales: Less Than 100 Thousand USD"
+    }
+    return "Global Sales: " + value + " Million USD"
+}
+
 // pack the data
 const pack = data => d3.pack()
     .size([width - 2, height - 2])
@@ -192,8 +200,6 @@ d3.csv(genreDataUrl).then((genreData) => {
             dataJoinBubbleChart()
         }
 
-        // const initialBarChart = () =>
-
         const dataJoinBarChart = () => {
             const x = 'value'
             const y = 'name'
@@ -204,21 +210,15 @@ d3.csv(genreDataUrl).then((genreData) => {
                 return startYear <= d.Year && d.Year <= endYear && (readableGenres.length === 0 || readableGenres.indexOf(d.Genre) > -1)
             })
             let publisherSalesData = sumByCol(dataFilt, 'Publisher', 'Global_Sales')
-            // console.log(publisherSalesData, 'pubbbb')
 
             // filter test
             const margin = ({ top: 100, right: 300, bottom: 100, left: 500 })
-            // const width = 1000
-            // const height = 300
 
             console.log(publisherSalesData)
 
             bar_xScale.domain([0, d3.max(publisherSalesData, d => d[x])])
-            // .range([margin.left, width - margin.right])
 
             bar_yScale.domain(d3.range(publisherSalesData.length))
-            // .rangeRound([margin.top, height - margin.bottom])
-            // .padding(0.1)
 
             const colorScale = d3.scaleOrdinal()
                 .domain(publisherSalesData.map(d => d[y]))
@@ -282,8 +282,6 @@ d3.csv(genreDataUrl).then((genreData) => {
                 .duration(1000)
                 .call(bar_yaxis
                     .tickFormat(i => publisherSalesData[i].name).tickSizeOuter(0))
-
-            console.log(svgBar.selectAll('.x-axis'), 'poopie')
         }
 
         dataJoinBarChart()
@@ -297,9 +295,9 @@ d3.csv(genreDataUrl).then((genreData) => {
 
 
 
-        document.getElementById('bubble').appendChild(svgBubble.node())
-
-        var bubbleTooltip = d3.select("#bubble")
+        var bubbleTooltip = 
+        // svgBubble
+        d3.select("#bubble")
             .append("div")
             .style("opacity", 0)
             .attr("class", "tooltip")
@@ -309,14 +307,19 @@ d3.csv(genreDataUrl).then((genreData) => {
             .style("border-radius", "5px")
             .style("padding", "10px")
 
+        document.getElementById('bubble').appendChild(svgBubble.node())
+
 
         const bubbleMouseOver = d => {
             // console.log('d-mouseover', d)
-            console.log(`translate(${d.x + 90}, ${(d.y + 90)})`)
+            // console.log(`translate(${d.x + 90}, ${(d.y + 90)})`)
             bubbleTooltip
-                .attr("transform", `translate(${d.x + 90}, ${(d.y + 90)})`)
+                // .attr("transform", `translate(${d.x + 90}, ${(d.y + 90)})`)
+                .style("left", d.x + 90 + "px")
+                .style("top", d.y + 90 + "px")
                 .style("opacity", 1)
                 .html("Global Sales: " + d.value.toFixed(1) + " Million USD")
+                .html(formTooltipString(d.value.toFixed(1)))
                 .transition()
         }
 
@@ -329,20 +332,20 @@ d3.csv(genreDataUrl).then((genreData) => {
         }
 
         const dataJoinBubbleChart = () => {
+            console.log("DJBC")
             // filter test
             const dataFiltBubble = dataFilt.filter(d => !selectedPublisher || (d.Publisher == selectedPublisher.name))
-            console.log(dataFilt.length, 'THE LENGTH')
             const gameSalesData = sumByCol(dataFiltBubble, 'Name', 'Global_Sales')
 
             // color by name
             const color = d3.scaleOrdinal(gameSalesData.map(d => d.name), d3.schemeSet3)
 
             // repack and run
-            console.log(gameSalesData)
             const root = pack(gameSalesData)
-            const leaves = root.leaves()
+            var leaves = root.leaves()
 
-            console.log(leaves)
+            // Don't make leaves for publishers without any games in time frame
+            if (gameSalesData.length == 0) {leaves = []}
 
             const t = svgBubble.transition()
                 .duration(750);
@@ -394,9 +397,9 @@ d3.csv(genreDataUrl).then((genreData) => {
                                 .attr("r", d => d.r)
                                 .attr("fill-opacity", 0.7)
                                 .attr("fill", d => color(d.data.name)))
-                        // .on('mouseover', (event, d) => {
-                        //     bubbleMouseOver(d)
-                        // })
+                        .on('mouseover', (event, d) => {
+                            bubbleMouseOver(d)
+                        })
 
                         update.select("text")
                             .selectAll("tspan")
@@ -408,9 +411,9 @@ d3.csv(genreDataUrl).then((genreData) => {
                             .attr('font-size', '8px')
                             .text(d => d.data.name)
                             .call(wrap, 100)
-                        // .on('mouseover', (event, d) => {
-                        //     bubbleMouseOver(d)
-                        // })
+                        .on('mouseover', (event, d) => {
+                            bubbleMouseOver(d)
+                        })
                     },
                     exit => exit.remove()
                 )
@@ -435,7 +438,7 @@ d3.csv(genreDataUrl).then((genreData) => {
 
                     // delete text
                     d3.selectAll('.selected')
-                        .text('None')
+                        .text('Top 10 Publishers')
                         .style('color', 'black')
 
                     // re-join
@@ -448,7 +451,7 @@ d3.csv(genreDataUrl).then((genreData) => {
             .attr('id', 'selected')
             .attr('class', 'selected')
             .style('font-family', 'sans-serif')
-            .text('None')
+            .text('Top 10 Publishers')
             .style('color', 'black')
 
         //////////
@@ -585,10 +588,6 @@ d3.csv(genreDataUrl).then((genreData) => {
         // What to do when one group is hovered
         var highlight = function (d) {
             if (!clickSelecting) {
-                // console.log(d)
-                // console.log("." + d.y.__data__)
-                // console.log("." + d.target.__data__)
-                // reduce opacity of all groups
                 d3.selectAll(".myArea").transition().style("opacity", .2)
                 d3.select("." + d.target.__data__).transition().style("opacity", 1)
             }
@@ -601,7 +600,7 @@ d3.csv(genreDataUrl).then((genreData) => {
             }
         }
 
-        var genreChoice = function (d) {
+        var shiftGenreChoice = function (d) {
             // reduce opacity of all groups
             let genreName = "." + d.target.__data__;
             if (selectedGenres.includes(genreName)) {
@@ -610,16 +609,10 @@ d3.csv(genreDataUrl).then((genreData) => {
 
                 if (!clickSelecting) {
                     d3.selectAll(".myArea").transition().duration(1250).style("opacity", .75);
-
-                    // Update Later Charts
-                    dataJoinBarChart()
-                    dataJoinBubbleChart()
+                    d3.selectAll(".legendSquare").transition().duration(1250).style("opacity", 1)
                 } else {
                     d3.select(genreName).transition().duration(1250).style("opacity", .1);
-
-                    // Update Later Charts
-                    dataJoinBarChart()
-                    dataJoinBubbleChart()
+                    d3.select(".legendSquare-" + d.target.__data__).transition().duration(10).style("opacity", .1)
                 }
 
             }
@@ -627,22 +620,40 @@ d3.csv(genreDataUrl).then((genreData) => {
                 if (clickSelecting) {
                     selectedGenres.push(genreName);
                     d3.select(genreName).transition().duration(10).style("opacity", 1);
-
-                    // Update Later Charts
-                    dataJoinBarChart()
-                    dataJoinBubbleChart()
+                    d3.select(".legendSquare-" + d.target.__data__).transition().duration(10).style("opacity", 1)
+        
                 }
                 else {
                     clickSelecting = true
                     selectedGenres.push(genreName)
                     d3.selectAll(".myArea").transition().duration(1250).style("opacity", .1)
                     d3.select("." + d.target.__data__).transition().duration(10).style("opacity", 1)
-
-                    // Update Later Charts
-                    dataJoinBarChart()
-                    dataJoinBubbleChart()
+                    d3.selectAll(".legendSquare").transition().duration(1250).style("opacity", .1)
+                    d3.select(".legendSquare-" + d.target.__data__).transition().duration(10).style("opacity", 1)
                 }
             }
+        }
+
+        var genreChoice = function (d) {
+            if (d.shiftKey) {
+                shiftGenreChoice(d)
+            }
+            else{
+                // reduce opacity of all groups
+                let genreName = "." + d.target.__data__;
+                clickSelecting = true
+                selectedGenres = []
+                selectedGenres.push(genreName)
+                d3.selectAll(".myArea").transition().duration(1250).style("opacity", .1)
+                d3.selectAll(".legendSquare").transition().duration(1250).style("opacity", .1)
+
+                d3.select("." + d.target.__data__).transition().duration(10).style("opacity", 1)
+                d3.select(".legendSquare-" + d.target.__data__).transition().duration(10).style("opacity", 1)
+            }
+            selectedPublisher = null
+            // Update Later Charts
+            dataJoinBarChart()
+            dataJoinBubbleChart()
         }
 
         //////////
@@ -658,15 +669,28 @@ d3.csv(genreDataUrl).then((genreData) => {
             .on('click', () => {
                 clickSelecting = false
                 selectedGenres = []
+
+                selectedPublisher = null
+
+                svgBar.selectAll('.bar')
+                    .style('opacity', 1)
+                    .transition()
+                    .delay(100)
+
+                // delete text
+                d3.selectAll('.selected')
+                    .text('Top 10 Publishers')
+                    .style('color', 'black')
+
                 startYear = defaultStartYear
                 endYear = defaultEndYear
                 areaChart.select('.brush').call(brush.clear);
 
-                // svg.selectAll(".brush").call(brush)
                 dataJoinBarChart()
                 dataJoinBubbleChart()
                 // reset all opacities
                 d3.selectAll(".myArea").transition().duration(1250).style("opacity", .75)
+                d3.selectAll(".legendSquare").transition().duration(1250).style("opacity", 1)
             })
 
         //////////
@@ -679,11 +703,13 @@ d3.csv(genreDataUrl).then((genreData) => {
             .data(keys)
             .enter()
             .append("rect")
+            .attr("class", d => `legendSquare legendSquare-${d}`)
             .attr("x", width)
             .attr("y", function (d, i) { return 10 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
             .attr("width", size)
             .attr("height", size)
             .style("fill", function (d) { return color(d) })
+            .style('cursor', 'pointer')
             .on("mouseover", highlight)
             .on("mouseout", noHighlight)
             .on("click", genreChoice)
@@ -700,6 +726,7 @@ d3.csv(genreDataUrl).then((genreData) => {
             .attr("text-anchor", "left")
             .style("alignment-baseline", "middle")
             .style('font-family', 'sans-serif')
+            .style('cursor', 'pointer')
             .on("mouseover", highlight)
             .on("mouseout", noHighlight)
             .on("click", genreChoice)
